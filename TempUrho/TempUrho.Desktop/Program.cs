0ab0efba69ae1ba62f7ad9f6093e46b51a01fc75
DESKTOP-0926CAP\Katsurugi <enzo2004@ukr.net>
2018-02-03 18:54:17 +0200

    basic urho

---
+++ TempUrho/TempUrho.Desktop/Program.cs
@@ -0,0 +1,12 @@
+﻿using Urho;
+
+namespace TempUrho.Desktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MyGame(new ApplicationOptions("Data")).Run();
+        }
+    }
+}
